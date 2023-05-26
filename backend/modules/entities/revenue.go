df5c992c41d7bdd2721aaddc91a8cc7d7bb20a82
PuNe <worachanon226@hotmail.com>
2023-05-26 14:10:01 +0700

    Update entitty-revenue

--- backend/modules/entities/revenue.go
+++ backend/modules/entities/revenue.go
@@ -16,3 +16,12 @@ type Revenue struct {
 	Value  int64     `json:"value" bson:"value"`
 	Type   string    `json:"type" bson:"type"`
 }
+
+type PostRevenueReq struct {
+	ID     string    `json:"id" bson:"_id"`
+	UserID string    `json:"userid" bson:"userid"`
+	Date   time.Time `json:"date" bson:"time"`
+	Title  string    `json:"title" bson:"title"`
+	Detail string    `json:"detail" bson:"detail"`
+	Value  int64     `json:"value" bson:"value"`
+}
