a0051f1f112f7997d2473527611db14114e63b63
PuNe <worachanon226@hotmail.com>
2023-05-27 20:33:39 +0700

    Update Lists

--- client/src/pages/Lists.jsx
+++ client/src/pages/Lists.jsx
@@ -2,41 +2,56 @@ import { useContext, useState, useEffect } from "react";
 import List from "./components/List";
 import { UserContext } from "../UserContext";
 import { getExpenses } from "../controller/ExpenseController";
+import { getRevenues } from "../controller/RevenueController";
 import Actionmodal from "./components/Actionmodal";
 
 const Lists = () => {
+  let lists = [];
   const { user } = useContext(UserContext);
   const [expenseList, setExpenseList] = useState([]);
+  const [revenueList, setRevenueList] = useState([]);
+  const [list, setList] = useState([]);
 
-  const getExp = (s) => {
-    console.log("Refresh");
+  const getLists = (s) => {
+    lists = [];
     getExpenses(s)
       .then((res) => {
         setExpenseList(res.data);
+        lists = res.data;
       })
       .catch((error) => {
         console.error("Error fetching expenses:", error);
       });
+
+    getRevenues(s)
+      .then((res) => {
+        setRevenueList(res.data);
+        lists = lists.concat(res.data);
+        setList(lists);
+      })
+      .catch((error) => {
+        console.error("Error fetching revense:", error);
+      });
   };
 
   useEffect(() => {
-    getExp(user.id);
+    getLists(user.id);
   }, [user.id]);
 
   return (
     <>
       <div className="bg-white dark:bg-gray-900 mt-16 h-screen">
         <div className="py-8 px-4 text-center lg:py-16">
-          <Actionmodal callback={getExp} />
+          <Actionmodal callback={getLists} />
 
           <div className="mt-5 flex flex-wrap justify-center">
-            {expenseList.map((expense) => (
+            {list.map((obj) => (
               <List
-                key={expense.id}
-                type="expense"
-                title={expense.title}
-                detail={expense.detail}
-                value={expense.value}
+                key={obj.id}
+                type={obj.type}
+                title={obj.title}
+                detail={obj.detail}
+                value={obj.value}
               />
             ))}
           </div>
