28c783142a91d6bef933256bfd3e72552357fe30
PuNe <worachanon226@hotmail.com>
2023-05-29 22:10:27 +0700

    Update BottomNav

--- client/src/pages/components/BottomNav.jsx
+++ client/src/pages/components/BottomNav.jsx
@@ -1,16 +1,32 @@
-const BottomNav = () => {
+const BottomNav = ({ expensesList, revenuesList }) => {
+  const totalExpenses = expensesList.reduce(
+    (total, expense) => total + expense.value,
+    0
+  );
+
+  const totalRevenues = revenuesList.reduce(
+    (total, revenue) => total + revenue.value,
+    0
+  );
+
   return (
     <>
       <div className="fixed bottom-0 left-0 z-50 grid w-full h-16 px-8 bg-gray-800 border-gray-700">
         <div className="flex items-center justify-center text-white ">
           <div className="basis-1/3 flex ml-4">
-            <div className="font-bold text-lg">Total Expenses: </div>
+            <div className="font-bold text-lg">
+              Total Expenses: {totalExpenses}
+            </div>
           </div>
           <div className="basis-1/3 flex ml-4">
-            <div className="font-bold text-lg">Total Revenses: </div>
+            <div className="font-bold text-lg">
+              Total Revenses: {totalRevenues}
+            </div>
           </div>
           <div className="basis-1/3 flex ml-4">
-            <div className="font-bold text-lg">Saving: </div>
+            <div className="font-bold text-lg">
+              Saving: {totalRevenues - totalExpenses}
+            </div>
           </div>
         </div>
       </div>
